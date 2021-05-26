49fabfd7aac305162ae830380d661d7eb5f2168c
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-05-25 20:12:29 -0600

    Creating the login

---
+++ src/Components/Login.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react"
+import "./style/login.css"
+
+class Login extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div className="Container__card">
+                    
+                    <form onSubmit={this.props.onSubmit} className="form"> 
+                        <img  className="Icon__image" src="https://image.flaticon.com/icons/png/128/4689/4689456.png" alt="Icon"/>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Email
+                            </label>
+                            <input
+                            type="email"
+                            className="form-control"
+                            name="email"
+                            />
+                        </div>
+
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Password
+                            </label>
+                            <input
+                            type="password"
+                            className="form-control"
+                            name="password"
+                            />
+                            <button type="submit" className="Submit__button mt-5">
+                                Login
+                            </button>
+                            <div className="row mb-5">
+                                <a className="Link__password ml-4">Forgot your password?</a>
+                            </div>
+                            
+
+                        </div>
+
+                    </form>
+                </div>
+                
+            </React.Fragment>
+        );
+    }
+}
+
+export default Login;
