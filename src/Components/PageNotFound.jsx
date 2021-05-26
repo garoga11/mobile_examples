35b18366c111569d41bf518f558eb843a21b03cb
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-05-25 20:13:01 -0600

    Creating 404 page

---
+++ src/Components/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import "./style/PageNotFound.css"
+
+class PageNotFound extends Component{
+    render(){
+        return(
+            <img className="Img" src="https://image.freepik.com/vector-gratis/ups-error-404-ilustracion-concepto-robot-roto_114360-5529.jpg" alt="404 page not found"/>
+        );
+    }
+
+}
+
+export default PageNotFound;
