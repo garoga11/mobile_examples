2a7852eed1af5f7675a3cffb91cce6bf1e12f73e
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-06-08 21:57:03 -0600

    Correcting the imports

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Hero from "../../Components/Hero"
 import Button from "../../Components/MainButton"
+import Footer from "../../Components/Footer"
 import "./main.css"
 
 const Main = (props) => {
@@ -9,11 +10,11 @@ const Main = (props) => {
             <div className="Main">
                 <div className="Main__content">
                     <h1 className="Main__title">{props.title || "Welcome"}</h1>
-                    <Button contentText={"Start"} link={"/badges"}></Button>
+                    <Button contentText={"Start"} theme={"Button"} link={"/badges"}></Button>
                 </div>
                 <Hero h={"88vh"}></Hero>
             </div>
-
+            <Footer s={{bottom:0}}> </Footer>
         </React.Fragment>
     );
 } 
