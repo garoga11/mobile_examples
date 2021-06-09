d04cd9a7426ed2bf537d5e635e31ed2458c5cf28
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-06-08 21:49:47 -0600

    Changing the footer style

--- src/Components/PageNotFound.jsx
+++ src/Components/PageNotFound.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react"
+import Footer from "./Footer"
 import "./style/PageNotFound.css"
 
-class PageNotFound extends Component{
-    render(){
-        return(
+const PageNotFound = () =>{
+    return(
+        <React.Fragment>
             <img className="Img" src="https://image.freepik.com/vector-gratis/ups-error-404-ilustracion-concepto-robot-roto_114360-5529.jpg" alt="404 page not found"/>
-        );
-    }
+            <Footer s={{bottom:0}}> </Footer>
+        </React.Fragment>
+    );     
 
 }
 
