5d90c0cd730f0563526bb9c726d823e8a9324c9c
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-06-08 21:46:40 -0600

    Correcting the footer style

--- src/Components/Badge.jsx
+++ src/Components/Badge.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HeaderBackground from "../images/badge_header.jpg"
 import ProfilePic from "../images/profile3.jpg"
+import Button from "../Components/MainButton"
 import "./style/Badge.css";
 
 
@@ -11,11 +12,11 @@ class Badge extends Component {
             <React.Fragment>
                 <div className="Badge m-5 flex-column">
                     <div className="Badge__header">
-                        <img src={this.props.header_picture} alt="Header background"></img>
+                        <img src={this.props.header_img_url} alt="Header background"></img>
                     </div>
                     <div className="Badge__userInfo container mt-5 mb-1">
                         <div className="Badge__userImage">
-                            <img src={this.props.profile_picture}  alt="Profile pic"></img>
+                            <img src={this.props.profile_picture_url}  alt="Profile pic"></img>
                         </div>
                         <h4 className="text-center mt-5">
                             {this.props.name} <i>{this.props.age}</i>
